78d3428ee5ea2d16bc49382fcf9e188054aa6b65
HeeteshKamthe <heeteshkamthe09@gmail.com>
2025-05-26 22:54:40 +0530

    Update LEMP.sh

--- LEMP/LEMP.sh
+++ LEMP/LEMP.sh
@@ -4,9 +4,7 @@ echo "Updating system..."
 sudo yum update -y
 
 echo "Installing Nginx, MariaDB, PHP..."
-sudo amazon-linux-extras enable nginx1 php8.0 mariadb10.5 -y
-sudo yum clean metadata
-sudo yum install -y nginx php php-fpm php-mysqlnd mariadb-server
+sudo yum install -y nginx php php-fpm php-mysqlnd mariadb105-server
 
 # Enable and start services
 sudo systemctl enable nginx
