f028bb7f2358391cffa0b0569eb899e6a92a78d9
HeeteshKamthe <heeteshkamthe09@gmail.com>
2025-05-26 22:57:02 +0530

    Update LAMP.sh

--- LAMP/LAMP.sh
+++ LAMP/LAMP.sh
@@ -2,9 +2,8 @@ echo "Updating system..."
 sudo yum update -y
 
 echo "Installing Apache, MariaDB, PHP..."
-sudo amazon-linux-extras enable php8.0 mariadb10.5 -y
-sudo yum clean metadata
-sudo yum install -y httpd php php-mysqlnd mariadb-server
+
+sudo yum install -y httpd php php-mysqlnd mariadb105-server
 
 # Enable and start services
 sudo systemctl enable httpd
